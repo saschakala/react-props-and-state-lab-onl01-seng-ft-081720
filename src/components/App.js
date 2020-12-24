7afd63b67fc360eeace1fb314635c0dcea8b026f
“saschakala” <mxsaschakala@gmail.com>
2020-12-24 12:53:39 -0500

    commit tree 44b4ca83f65d53319e65ad4c461eae480468ea12

--- src/components/App.js
+++ src/components/App.js
@@ -15,6 +15,28 @@ class App extends React.Component {
     }
   }
 
+  onChangeType = (selectedType) => {
+    this.setState({
+      filters: {type: selectedType}
+    })
+  }
+
+  onFindPetsClick = () => {
+    let endpoint = '/api/pets'
+    if (this.state.filters.type !== 'all') {
+      endpoint += `?type=${this.state.filters.type}`
+    }
+
+    fetch(endpoint)
+    .then(resp => resp.json())
+    .then(pets => this.setState({pets: pets}))
+  }
+
+  onAdoptPet = (id) =>{
+    const pet = this.state.pets.find(pet => pet.id === id);
+    pet.isAdopted = true;
+  }
+
   render() {
     return (
       <div className="ui container">
@@ -24,10 +46,12 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters 
+              onChangeType={this.onChangeType}
+              onFindPetsClick={this.onFindPetsClick}/>
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser pets={this.state.pets} onAdoptPet={this.onAdoptPet}/>
             </div>
           </div>
         </div>
